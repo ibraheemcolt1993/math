de911f56b395ca66193cb4b029541f9be73c67da
ibraheemcolt1993 <ibraheem.ahmed.ya@gmail.com>
2026-01-12 14:58:48 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,8 @@
-console.log("JS شغال ✅");
-
+document.getElementById("btn").onclick = function(){
+  const id = document.getElementById("nid").value.trim();
+  if(!id){
+    alert("أدخل رقم الهوية");
+    return;
+  }
+  alert("أهلًا بك، رقمك: " + id);
+};
