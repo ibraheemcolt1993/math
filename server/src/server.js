cd74f73f6ab229ce33b0844796ac109c890d37eb
ibraheemcolt1993 <ibraheem.ahmed.ya@gmail.com>
2026-01-21 00:01:28 +0200

    Document removed files

--- server/src/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const app = require('./app');
-
-const port = Number(process.env.PORT || 3000);
-
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
-});
